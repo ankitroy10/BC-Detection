dc2ed8627c7dab8af78a55ed512ecbb6ebfc64f3
Ankit Roy <ankit@Ankits-MacBook-Pro.local>
2020-01-25 13:27:39 -0500

    Initial Commit

---
+++ BC Detect/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BC Detect
+//
+//  Created by Ankit Roy on 1/25/20.
+//  Copyright © 2020 Ankit Roy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
